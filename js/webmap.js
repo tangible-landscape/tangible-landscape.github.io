b077a57b314be556c4de376dfca4d8f6cc803371
Brendan <brendan.harmon@gmail.com>
2023-10-23 21:59:02 -0500

    updated webmap

--- js/webmap.js
+++ js/webmap.js
@@ -1,6 +1,11 @@
 // create map
 var map = L.map('map').setView([35.7818,-78.6764], 3);
-L.tileLayer.provider('Stamen.TonerLite').addTo(map);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(mymap);
 
 // create system markers
 var markerIcon = new L.Icon({
